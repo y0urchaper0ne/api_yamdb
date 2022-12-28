152cf6784d233e6b0f1884243b40ec36d198f0b2
Илья Никитин <ilyanikitin2308@gmail.com>
2022-12-28 21:21:16 +0300

    добавлена модель User

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+# class Roles(models.TextChoices):
+#     USER = 'user',
+#     MODERATIOR = 'moderator',
+#     ADMIN = 'admin'
+
+CHOICES = (
+    ('User', 'зарегистрированный пользователь'),
+    ('Moderator', 'модератор'),
+    ('Admin', 'администратор'),
+)
+
+
+class User(AbstractUser):
+    bio = models.TextField(
+        'Биография',
+        null=True,
+        blank=True,
+    ),
+    email = models.EmailField(
+        null=False,
+        blank=False,
+    ),
+    role = models.CharField(
+        max_length=16,
+        choices=CHOICES,
+        #choices=Roles.choices,
+        #default=Roles.USER,
+    )
+
+
+class Title(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Genre(models.Model):
+    pass
